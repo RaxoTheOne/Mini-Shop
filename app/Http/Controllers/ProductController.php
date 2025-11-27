ae6f70c6ae5e5570042fba4ca0a86de5a2364270
RaxoTheOne <traxon.bg@gmail.com>
2025-11-27 23:08:39 +0100

    functions add, remove und clear erstellt

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -17,7 +17,51 @@ class ProductController extends Controller
             ->paginate(12);
 
         $categories = Category::all();
-        
+
         return view('products.index', compact('products', 'categories'));
+    }
+
+    // Produkt zum Warenkorb hinzufügen
+    public function add($id)
+    {
+        $product = Product::where('id', $id)
+            ->where('is_active', true)
+            ->firstOrFail();
+
+        $cart = session()->get('cart', []);
+
+        if (isset($cart[$id])) {
+            $cart[$id]['quantity']++;
+        } else {
+            $cart[$id] = [
+                'product' => $product,
+                'quantity' => 1,
+            ];
+        }
+
+        session()->put('cart', $cart);
+
+        return redirect()->back()->with('success', 'Produkt zum Warenkorb hinzugefügt');
+    }
+
+    // Produkt aus dem Warenkorb entfernen
+    public function remove($id)
+    {
+        $cart = session()->get('cart', []);
+
+        if (isset($cart[$id])) {
+            unset($cart[$id]);
+            session()->put('cart', $cart);
+        }
+
+        return redirect()->route('cart.index')->with('success', 'Produkt aus dem Warenkorb entfernt!');
+    }
+
+    // Warenkorb leeren
+    public function clear()
+    {
+        session()->forget('cart');
+
+        return redirect()->route('cart.index')->with('success', 'Warenkorb geleert!');
     }
 }
